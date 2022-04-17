8e1368897b7fd70b441a97daf6ffa1b208cb1a64
Alejandro González Reyes <alexglzrys@gmail.com>
2022-04-17 14:00:31 -0500

    agregar esquemas de Angular Material

--- README.md
+++ README.md
@@ -14,3 +14,17 @@ ng add @angular/material
 Esto instalará **Angular Material** sobre nuestro proyecto de Angular, así como el **CDK (Component Development Kit)** y **Angular Animation**.
 
 Como parte de la instalación, el asistente preguntará sobre el tema (conjunto de estilos visuales) a usar en la aplicación, si se desea establecer la tipografía usada por Angular Material de forma global en la aplicación (fuente e iconos), así como configurar las animaciones del navegador para Angular Material; este último de suma importancia para poder proyectar los efectos visuales incorporados en la mayoría de los componentes de dicha librería.
+
+#### Angular Material Schematics
+
+Angular Material viene empaquetado con un generador automático de esquemas (scafolding) **Angular CLI schematics** para nuestras aplicaciones de Angular, son componentes preconstruidos a partir de componentes de Angular Material los cuales se pueden crear a traves del siguiente comando
+
+```
+ng generate @angular/material:tipo-esquema nombreComponente
+```
+
+Entre los esquemas que se pueden generar tenemos
+
+- address-form, address-form, address-form, tree, table, address-form	
+
+Por buenas prácticas, se recomienda agrupar este tipo de componentes bajo un módulo que gestione todo lo relacionado a Angular Material, e importarlo según corresponda en nuestra aplicación
--- src/app/material/material.module.ts
+++ src/app/material/material.module.ts
@@ -2,18 +2,40 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { MatButtonModule } from "@angular/material/button";
 import { MatIconModule } from "@angular/material/icon";
+import { DashboardComponent } from './components/dashboard/dashboard.component';
+import { MatGridListModule } from '@angular/material/grid-list';
+import { MatCardModule } from '@angular/material/card';
+import { MatMenuModule } from '@angular/material/menu';
+import { LayoutModule } from '@angular/cdk/layout';
+import { TableComponent } from './components/table/table.component';
+import { MatTableModule } from '@angular/material/table';
+import { MatPaginatorModule } from '@angular/material/paginator';
+import { MatSortModule } from '@angular/material/sort';
+import { TreeComponent } from './components/tree/tree.component';
+import { MatTreeModule } from '@angular/material/tree';
 
 
 @NgModule({
-  declarations: [],
+  declarations: [DashboardComponent, TableComponent, TreeComponent],
   imports: [
     CommonModule,
     MatButtonModule,
-    MatIconModule
+    MatIconModule,
+    MatGridListModule,
+    MatCardModule,
+    MatMenuModule,
+    LayoutModule,
+    MatTableModule,
+    MatPaginatorModule,
+    MatSortModule,
+    MatTreeModule
   ],
   exports: [
     MatButtonModule,
     MatIconModule,
+    DashboardComponent,
+    TableComponent,
+    TreeComponent
   ]
 })
 export class MaterialModule { }
